fb9f6c0e4da81238e8b6c7889cf453d9173924ec
Jeffrey M. Brown <brown.jeffreym@gmail.com>
2010-11-10 14:26:54 -0700

    Updated parse() function. parse() now can find regex matches in a string, separate the matches, and finally order the matches based on their location in the original string. parse() output is a list of string tokens. Also added a list of strings for regex matching of Bra and Ket. parse() now passes iostreamTest for strings only including Bra and Ket.

--- iostream.py
+++ iostream.py
@@ -1,6 +1,9 @@
 """Module used to for string and determine if it is a valid
 expression of dirac notation in quantum mechanics."""
 
+import re
+import QMTypes
+
 # Define exceptions
 class InputError(Exception):
     def __init__(self, string):
@@ -8,10 +11,31 @@ class InputError(Exception):
     def __str__(self):
         return repr(self.msg)
 
+
+regexList = ['(<[a-zA-Z][a-zA-Z0-9]*[|])', # Bra
+             '([|][a-zA-Z][a-zA-Z0-9]*>)'] # Ket
+
 def parse(s):
     """Parses string s into valid string tokens, eg Bra, Ket, number, etc."""
-    pass
+    tokenList = []
+    for regex in regexList:
+        tokenMatches = re.finditer(regex, s) # find all matches in string
+        for eachMatch in tokenMatches:
+            loc = eachMatch.span() # location of match: (begin, end)
+            token = s[loc[0]:loc[1]]
+            tokenList.append([loc, token])
+
+    tokenList.sort() # sort in place by location returned from span()
+    tokens = [item[1] for item in tokenList]
+    return tokens
 
 def assemble(tokenList):
     """Assembles valid tokens into an output string."""
     pass
+
+
+if __name__ == '__main__':
+    test = '<a|b><c|d>'
+    t = ['<a|', '|b>', '<c|', '|d>']
+
+    print parse(test)
