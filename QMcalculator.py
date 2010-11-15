68998b2041d3ff3cf0dfeda861847b1b19d819fa
Jeffrey M. Brown <brown.jeffreym@gmail.com>
2010-11-14 23:20:59 -0700

    Changed this file to be a command line program of the current implementation of program. Run this file with: python QMcalculator.py.

--- QMcalculator.py
+++ QMcalculator.py
@@ -1,10 +1,61 @@
-"""Quantum mechanics calculator"""
+"""Quantum mechanics calculator
+
+Run using:
+
+python QMcalculator.py
+
+
+"""
 
 import QMTypes
 import classify
+import iostream
 
+class CommandLinePrompt(object):
+    """Current interface to the quantum mechanics calculator."""
+    def __init__(self, program):
+        self.history = []
+        self.main = program
+
+    def run(self):
+        """Begin the command line program"""
+        print """
+#    Quantum Mechanics Calculator 
+#
+#    Copyright (C) 2010  Jeffrey M. Brown, Kyle T. Taylor
+#
+#    Type 'exit' to quit.
+     
+"""
+        while 1:
+            line = raw_input('> ') # get a line from the prompt
+            self.history.append(line) # keep track of commands
+            if line == 'exit': break
+
+            try:
+                self.main(line)
+            except iostream.InputError, classify.ClassificationError:
+                print "Invalid input"
+
+def main(line):
+    c = classify.Classifier(QMTypes.inputDict, QMTypes.outputDict)
+    
+    inputTokenList = iostream.parse(line) # parse input
+    classList = []
+    for token in inputTokenList:
+        qmClass = c.toClass(token) # convert input into classes
+        classList.append(qmClass)
+    print classList # display all classes that were identified
+
+    outputTokenList = []
+    for qmClass in classList:
+        token = c.toToken(qmClass) # create token for each class
+        outputTokenList.append(token)
+    outputString = iostream.assemble(outputTokenList)
+    print outputString, '\n'
+
+            
 
 if __name__ == '__main__':
-    c = classify.Classifier(QMTypes.inputDict, QMTypes.outputDict)
-    ket = c.toClass('|a>')
-    print c.toToken(ket)
+    CLP = CommandLinePrompt(main)
+    CLP.run()
